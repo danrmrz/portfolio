9c12b2e741e2b52a404f0dee237d4d782075632d
Dan Ramirez <39846658+danrmrz@users.noreply.github.com>
2020-08-05 06:22:46 -0500

    Creado menu del sitio

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,47 @@
 import React from 'react'
 
 import Rotated from './Rotated'
-import menu from '../../assets/static/menu.png'
+import Menu from './Menu'
+import menu_icon from '../../assets/static/menu.png'
 
 import '../../assets/styles/components/Header.styl'
 
-const Header = () => {
-  return(
-    <Rotated className='header'>
-      <button className='header__menu'>
-        <img src={menu} className='header__menu-image' alt='Botón de menú'/>
-      </button>
-    </Rotated>
-  )
+class Header extends React.Component {
+  state = {
+    menuIsOpen: false
+  }
+
+  handleOpenMenu = () => {
+    this.setState({ menuIsOpen: true })
+  }
+
+  handleCloseMenu = () => {
+    const menu = document.getElementById('menu')
+    const menuContainer = document.getElementById('menu-container')
+    menu.classList.add('close')
+    menuContainer.classList.add('close')
+    this.timeoutId = setTimeout(() => {
+      this.setState({ menuIsOpen: false })
+    }, 980);
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timeoutId)
+  }
+
+  render(){
+    return(
+      <Rotated className='header'>
+        <button className='header__menu-button btn' onClick={this.handleOpenMenu}>
+          <img src={menu_icon} className='header__menu-image btn-img' alt='Botón de menú'/>
+        </button>
+        <Menu
+          isActive={this.state.menuIsOpen}
+          onClose={this.handleCloseMenu}
+        />
+      </Rotated>
+    )
+  }
 }
 
 export default Header
---
+++ src/components/Menu.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Link } from 'react-router-dom'
+
+import close_icon from '../../assets/static/close-w.png'
+import gitHub_icon from '../../assets/static/github-w.png'
+import linkedIn_icon from '../../assets/static/linkedin-w.png'
+import twitter_icon from '../../assets/static/twitter-w.png'
+
+import '../../assets/styles/components/Menu.styl'
+
+const gitHubLink = 'https://github.com/danrmrz'
+const linkedInLink = 'https://linkedin.com/in/danrmrz'
+const twitterLink = 'https://twitter.com/soydanrmrz'
+
+const Menu = (props) => {
+  if (!props.isActive) {
+    return(
+      null
+    )
+  }
+  return ReactDOM.createPortal(
+    <div className='menu-container' id='menu-container'>
+      <div className='menu' id='menu'>
+        <div className='menu__button-container'>
+          <button className='menu__button btn' onClick={props.onClose}>
+            <img src={close_icon} alt='' className='menu__button-image btn-img'/>
+          </button>
+        </div>
+
+        <div className='menu__links'>
+          <Link to='/' className='menu__links--titles' onClick={props.onClose}>
+            Inicio
+          </Link>
+          <Link to='/' className='menu__links--titles' onClick={props.onClose}>
+            Acerca de mi
+          </Link>
+        </div>
+
+        <div className='menu__social'>
+          <span className='menu__links--titles'>
+            Mis redes
+          </span>
+          <div className='menu__social-links'>
+            <a href={gitHubLink} target='_blank'>
+              <img src={gitHub_icon} alt='icono de gitHub' className='menu__social-links--images' />
+            </a>
+            <a href={linkedInLink}  target='_blank'>
+              <img src={linkedIn_icon} alt='icono de linkedIn' className='menu__social-links--images' />
+            </a>
+            <a href={twitterLink}  target='_blank'>
+              <img src={twitter_icon} alt='icono de twitter' className='menu__social-links--images' />
+            </a>
+          </div>
+        </div>
+
+        <div className='menu__made'>
+          <span>🐱‍💻 Daniel Ramirez 2020</span>
+        </div>
+      </div>
+      
+      <div className='clickToClose' onClick={props.onClose}></div>
+    </div>,
+    document.getElementById('modal')
+  )
+}
+
+export default Menu
