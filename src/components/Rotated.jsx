001c2a62ddf5072e5b6430a5a7ff145e54087796
Dan Ramirez <39846658+danrmrz@users.noreply.github.com>
2020-09-04 04:38:28 -0500

    Cambios en estilos del componente Rotated

--- src/components/Rotated.jsx
+++ src/components/Rotated.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 import '../assets/styles/components/Rotated.styl'
 
 const Rotated = ({ className, children }) => {
-  className ?
-    className = ` ${className}` :
-    className = ''
+  className
+  ? className = ` ${className}`
+  : className = ''
   return(
     <div className={ `rotated${className}` }>
       <div className='rotated--color'></div>
